a9d50ce3fb7359c92e7ca1fcc1512b1d1b39849e
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2022-12-04 10:40:22 +0100

    day4 two stars

--- src/main/day04/day04.kt
+++ src/main/day04/day04.kt
@@ -1,18 +1,35 @@
 package day04
 
 import readInput
+import second
 
-fun part1(input: List<String>): Int {
-    return 0
-}
+fun part1(input: List<String>): Int = input.solve(IntRange::fullyContains)
 
-
-fun part2(input: List<String>): Int {
-    return 0
-}
+fun part2(input: List<String>): Int = input.solve(IntRange::overlapsWith)
 
 fun main() {
     val input = readInput("main/day04/Day04")
     println(part1(input))
     println(part2(input))
 }
+
+private fun List<String>.solve(testFunction: (IntRange, IntRange) -> Boolean) =
+    map {
+        it.toRanges()
+    }.count {
+        testFunction(it.first, it.second)
+    }
+
+fun String.toRanges(): Pair<IntRange, IntRange> {
+    return split(",").let {
+        it.first().split("-").let { range -> range.first().toInt()..range.second().toInt() } to
+                it.second().split("-")
+                    .let { range -> range.first().toInt()..range.second().toInt() }
+    }
+}
+
+fun IntRange.fullyContains(other: IntRange) =
+    contains(other.first) && contains(other.last)
+
+fun IntRange.overlapsWith(other: IntRange) =
+    contains(other.first) || contains(other.last)
