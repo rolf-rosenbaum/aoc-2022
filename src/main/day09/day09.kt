54832d08b7f48248dc49cdb30b2c7c3494713469
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2022-12-09 16:25:11 +0100

    day9 a bit of cleanup

--- src/main/day09/day09.kt
+++ src/main/day09/day09.kt
@@ -3,13 +3,13 @@ package day09
 import Point
 import readInput
 
+typealias Knot = Point
+
+enum class Direction { R, U, D, L }
+
 val regex = """([RUDL]) (\d+)""".toRegex()
 
-enum class Direction {
-    R, U, D, L
-}
-
-val start = Point(0, 0)
+val start = Knot(0, 0)
 
 fun main() {
     val input = readInput("main/day09/Day09")
@@ -28,11 +28,11 @@ fun part2(input: List<String>): Int {
         .tailPositions(Rope(listOf(start, start, start, start, start, start, start, start, start, start))).size
 }
 
-private fun List<Direction>.tailPositions(rope: Rope): MutableSet<Point> {
-    val touchedPositions = mutableSetOf<Point>()
+private fun List<Direction>.tailPositions(rope: Rope): MutableSet<Knot> {
+    val touchedPositions = mutableSetOf<Knot>()
     var tmp = rope
     touchedPositions.add(rope.tail())
-    map {
+    forEach {
         tmp = tmp.step(it)
         touchedPositions.add(tmp.tail())
     }
@@ -51,34 +51,32 @@ fun List<String>.toSteps(): List<Direction> {
     return directions
 }
 
-data class Rope(val points: List<Point> = emptyList()) {
+data class Rope(val knots: List<Knot> = emptyList()) {
     fun step(direction: Direction): Rope {
         var newRope = Rope().add(head().move(direction))
-        points.drop(1).forEach {
-            val newHead = newRope.tail()
-            newRope = if (it.isAdjacentTo(newHead)) {
-                newRope.add(it)
-            } else {
-                val dx = it.x - newHead.x
-                val dy = it.y - newHead.y
-                val newX = if (dx < 0) it.x + 1 else if (dx > 0) it.x - 1 else it.x
-                val newY = if (dy < 0) it.y + 1 else if (dy > 0) it.y - 1 else it.y
-                newRope.add(Point(newX, newY))
+        knots.drop(1).forEach { knot ->
+            val previousKnot = newRope.tail()
+            newRope = if (knot.isAdjacentTo(previousKnot)) newRope.add(knot) else {
+                val dx = knot.x - previousKnot.x
+                val dy = knot.y - previousKnot.y
+                val newX = if (dx < 0) knot.x + 1 else if (dx > 0) knot.x - 1 else knot.x
+                val newY = if (dy < 0) knot.y + 1 else if (dy > 0) knot.y - 1 else knot.y
+                newRope.add(Knot(newX, newY))
             }
         }
         return newRope
     }
 
-    private fun head() = points.first()
-    fun tail(): Point = points.last()
-    private fun add(point: Point) = copy(points = points + point)
+    private fun head() = knots.first()
+    fun tail(): Knot = knots.last()
+    private fun add(knot: Knot) = copy(knots = knots + knot)
 }
 
-fun Point.isAdjacentTo(other: Point): Boolean {
+fun Knot.isAdjacentTo(other: Knot): Boolean {
     return other.x in (x - 1..x + 1) && other.y in (y - 1..y + 1)
 }
 
-fun Point.move(direction: Direction) =
+fun Knot.move(direction: Direction) =
     when (direction) {
         Direction.R -> copy(x = x + 1)
         Direction.U -> copy(y = y + 1)
