cc741ecf26aff7238ddf396766e5eefaad28e959
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2022-12-10 15:06:30 +0100

    prep for next 3 days

---
+++ src/main/day13/day13.kt
@@ -0,0 +1,18 @@
+package day13
+
+import readInput
+
+fun main() {
+    val input = readInput("main/day13/Day13")
+
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part1(input: List<String>): Int {
+    return 0
+}
+
+fun part2(input: List<String>): Int {
+    return 0
+}
