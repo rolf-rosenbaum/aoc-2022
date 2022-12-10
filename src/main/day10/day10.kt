63cd2f0d3e3e8578d1ec90a965dbe930de953243
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2022-12-10 14:30:25 +0100

    day 10 two stars

--- src/main/day10/day10.kt
+++ src/main/day10/day10.kt
@@ -3,18 +3,43 @@ package day10
 import readInput
 
 fun main() {
-    val input = readInput("main/day05/Day05")
+    val input = readInput("main/day10/Day10")
 
     println(part1(input))
-    println(part2(input))
+    part2(input)
 }
 
 fun part1(input: List<String>): Int {
+    val cycles = input.toCycles()
+    val interestingCycles = listOf(20, 60, 100, 140, 180, 220)
 
-    return 0
+    return interestingCycles.sumOf { cycles[it - 1] * it }
 }
 
-fun part2(input: List<String>): Int {
+fun part2(input: List<String>) {
+    val cycleMap = input.toCycles()
 
-    return 0
+    val crt = cycleMap.mapIndexed { index, register ->
+        val sprite = register - 1..register + 1
+        if (index % 40 in sprite) "#" else " "
+    }
+
+    crt.forEachIndexed { index, s ->
+        print(s)
+        if (index % 40 == 39) println()
+    }
+}
+
+private fun List<String>.toCycles(): List<Int> {
+    var x = 1
+    val cycles = mutableListOf<Int>()
+    forEach {
+        if (it.startsWith("noop")) {
+            cycles.add(x)
+        } else {
+            repeat(2) { cycles.add(x) }
+            x += it.split(" ").last().toInt()
+        }
+    }
+    return cycles
 }
