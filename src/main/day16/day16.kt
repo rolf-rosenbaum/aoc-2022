bbff1d3af475099cae3186536a92c119e4b2dd00
Rolf Rosenbaum <rolf.rosenbaum@rewe-digital.com>
2022-12-16 20:44:00 +0100

    Day 16 two stars

--- src/main/day16/day16.kt
+++ src/main/day16/day16.kt
@@ -1,6 +1,19 @@
 package day16
 
+import kotlin.math.max
 import readInput
+
+typealias Tunnels = Map<String, Valve>
+
+val flowRegex = """(\d+)""".toRegex()
+val valveRegex = """[A-Z]{2}""".toRegex()
+
+var totalTime = 30
+const val START = "AA"
+var maxPressureRelease = 0
+
+var allValves: Tunnels = mapOf()
+var shortestPaths: MutableMap<String, MutableMap<String, Int>> = mutableMapOf()
 
 fun main() {
     val input = readInput("main/day16/Day16_test")
@@ -9,10 +22,74 @@ fun main() {
 }
 
 fun part1(input: List<String>): Int {
-    return 0
+    prepareSearch(input)
+
+    checkAllPaths(0, START, emptySet(), 0)
+    return maxPressureRelease
 }
 
-fun part2(input: List<String>): Long {
-    return -1
+fun part2(input: List<String>): Int {
+    totalTime = 26
+    maxPressureRelease = 0
+    prepareSearch(input)
+
+    checkAllPaths(0, START, emptySet(), 0, true)
+    return maxPressureRelease
 }
 
+private fun prepareSearch(input: List<String>) {
+    val valves = input.map { it.parse() }
+
+    allValves = valves.associateBy { it.id }
+    shortestPaths =
+        shortestPathsFromEachTunnelToAllOtherTunnels(valves.associate { it.id to it.neighbouringValves.associateWith { 1 }.toMutableMap() }
+            .toMutableMap())
+}
+
+private fun checkAllPaths(currentPressureRelease: Int, currentValveId: String, visited: Set<String>, time: Int, withElefant: Boolean = false) {
+    maxPressureRelease = max(maxPressureRelease, currentPressureRelease)
+    shortestPaths[currentValveId]!!.forEach { (valveId, distance) ->
+        if (!visited.contains(valveId) && time + distance + 1 < totalTime) {
+            checkAllPaths(
+                currentPressureRelease = currentPressureRelease + (totalTime - time - distance - 1) * allValves[valveId]?.flow!!,
+                currentValveId = valveId,
+                visited = visited + valveId,
+                time = time + distance + 1,
+                withElefant
+            )
+        }
+    }
+    if (withElefant) {
+        checkAllPaths(currentPressureRelease, START, visited, 0, false)
+    }
+}
+
+fun String.parse(): Valve {
+    val valves = valveRegex.findAll(this).map { it.groupValues.first() }.toList()
+    val flow = flowRegex.findAll(this).first().groupValues.first().toInt()
+    val tunnels = valves.drop(1)
+    return Valve(id = valves.first(), flow = flow, neighbouringValves = tunnels)
+}
+
+private fun shortestPathsFromEachTunnelToAllOtherTunnels(shortestPaths: MutableMap<String, MutableMap<String, Int>>): MutableMap<String, MutableMap<String, Int>> {
+    shortestPaths.keys.forEach { a ->
+        shortestPaths.keys.forEach { b ->
+            shortestPaths.keys.forEach { c ->
+                val ab = shortestPaths[b]?.get(a) ?: 100
+                val ac = shortestPaths[a]?.get(c) ?: 100
+                val bc = shortestPaths[b]?.get(c) ?: 100
+                if (ab + ac < bc)
+                    shortestPaths[b]?.set(c, ab + ac)
+            }
+        }
+    }
+    shortestPaths.values.forEach {
+        it.keys.mapNotNull { key -> if (allValves[key]?.flow == 0) key else null }
+            .forEach { toRemove ->
+                it.remove(toRemove)
+            }
+    }
+    return shortestPaths
+}
+
+data class Valve(val id: String, val flow: Int, val neighbouringValves: List<String>)
